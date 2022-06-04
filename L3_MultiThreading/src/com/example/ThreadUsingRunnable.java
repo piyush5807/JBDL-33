828d17b47d8deb0b4a34d5db24152fa40c71e722
Piyush <piyushag@zeta.tech>
2022-06-04 15:32:16 +0530

    added L3 code

---
+++ L3_MultiThreading/src/com/example/ThreadUsingRunnable.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.util.function.Function;
+
+public class ThreadUsingRunnable implements Runnable {
+
+    public static void main(String[] args) {
+
+        System.out.println("Inside main function, thread  = " + Thread.currentThread());
+        ThreadUsingRunnable threadUsingRunnable = new ThreadUsingRunnable();
+
+        Thread thread = new Thread(threadUsingRunnable);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Inside run function of thread - " + Thread.currentThread());
+    }
+}
